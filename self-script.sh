c53b1f3efb062e84bbd3deb16bdf792b521c6e38
root <root@localhost.localdomain>
2022-09-18 18:46:45 +0800

    create self* as GraphMAE based model

---
+++ self-script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# for r in 0.0025 0.001 0.005 0.01
+for r in 0.0025
+do
+    file='res/arxiv/'$r'_res.txt'
+    python train_self_supervised.py --dataset ogbn-arxiv --nlayers=2 --sgc=1 --lr_feat=0.01 --gpu_id=0  --lr_adj=0.01 --r=${r}  --seed=1 --inner=3  --epochs=1000  --save=1 >> $file
+done
